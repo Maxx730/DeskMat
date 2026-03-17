a1bdca3a691763d2e85deda869728f1de6894e2f
Maxx730 <max.kinghorn@gmail.com>
2026-03-17 12:42:33 -0400

    Initial Commit

---
+++ DeskMat/DeskMatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskMatApp.swift
+//  DeskMat
+//
+//  Created by John Kinghorn on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DeskMatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeskMatTests/DeskMatTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeskMatTests.swift
+//  DeskMatTests
+//
+//  Created by John Kinghorn on 3/17/26.
+//
+
+import Testing
+@testable import DeskMat
+
+struct DeskMatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
